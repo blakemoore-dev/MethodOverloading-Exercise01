385b9c166d96cd38734f8e2dacc869537cb5f3d5
lostinthestack <lostinthestack@protonmail.com>
2021-10-07 10:53:17 -0500

    Init

---
+++ MethodOverloading-Exercise01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MethodOverloading_Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Add(1, 0, false));
+        }
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static string Add(int x, int y, bool a)
+        {
+            if (a)
+            {
+                return (x + y) > 1 ? $"{x+y} dollars" : $"1 dollar";
+            }
+            return "I guess you didn't want to add the numbers.";
+        }
+    }
+}
